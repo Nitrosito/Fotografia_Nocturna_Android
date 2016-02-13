eef38328725ad5153577bcbda0450beb1afe7915
nitrosito <nitrosito@gmail.com>
2016-02-13 12:36:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/nitrosito/utilesnocturna/Principal.java
@@ -0,0 +1,40 @@
+package com.example.nitrosito.utilesnocturna;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Principal extends AppCompatActivity implements View.OnClickListener {
+    EditText infmulti;
+    EditText indfocal;
+    TextView maxtiempo;
+    Button calcular;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        infmulti = (EditText) findViewById(R.id.fmultiplicacion);
+        indfocal = (EditText) findViewById(R.id.dfocal);
+        maxtiempo = (TextView) findViewById(R.id.maxexpo);
+        calcular = (Button) findViewById(R.id.texposicion);
+
+        calcular.setOnClickListener(this);
+    }
+
+    public void onClick(View control_pulsado) {
+        double multiplicador = Double.valueOf(infmulti.getText().toString());
+        double distancia = Double.valueOf(indfocal.getText().toString());
+        double res = 500/(multiplicador * distancia);
+        String ress = String.valueOf(res);
+        maxtiempo.setText(String.format("%.2f", res));
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(control_pulsado.getWindowToken(), 0);
+    }
+
+}
